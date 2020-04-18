e2f15f96014139532cf40a3e5055b52b98e44bbd
KingVanti <kingvanti650@gmail.com>
2020-04-18 19:40:15 +0200

    Create InitialRoom class

---
+++ Assets/Scripts/Game/World/InitialRoom.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.World
+{
+
+    /// <summary>
+    /// The room the game starts in
+    /// </summary>
+    public class InitialRoom : Room
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The bed the player starts in
+        /// </summary>
+        public ISpawnPoint Bed { get; private set; }
+
+        #endregion
+
+        #region Constructors
+
+        public InitialRoom(string name, GameObject roomGameObject) : base(name, roomGameObject)
+        {
+            Bed = roomGameObject.GetComponentInChildren<ISpawnPoint>();
+        }
+
+        #endregion
+
+    }
+
+}
