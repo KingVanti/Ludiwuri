4ac4e7331a0f3f833672c1eb648458ab20078d7e
KingVanti <kingvanti650@gmail.com>
2020-04-18 15:58:20 +0200

    Create RoomManager class

---
+++ Assets/Scripts/Game/World/RoomManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.World
+{
+
+    /// <summary>
+    /// Manages the loading of rooms
+    /// </summary>
+    public class RoomManager : MonoBehaviour
+    {
+
+    }
+
+}
