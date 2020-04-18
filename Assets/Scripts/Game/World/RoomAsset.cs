64e3513b139f29490fd23aedc756420619984a9f
KingVanti <kingvanti650@gmail.com>
2020-04-18 15:48:56 +0200

    Create RoomAsset class

---
+++ Assets/Scripts/Game/World/RoomAsset.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.World
+{
+
+    /// <summary>
+    /// Stores information about a room
+    /// </summary>
+    [CreateAssetMenu(menuName = "Ludiwuri/Room", fileName = "Unnamed Room")]
+    public class RoomAsset : ScriptableObject
+    {
+
+    }
+
+}
