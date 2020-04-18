c0f412a9f1d3ea8d10258209215ce97c4270aa15
KingVanti <kingvanti650@gmail.com>
2020-04-18 12:28:33 +0200

    Create PlayerMovementController script

---
+++ Assets/Scripts/Game/Player/PlayerMovementController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.Player
+{
+
+    /// <summary>
+    /// Controls the players movement
+    /// </summary>
+    public class PlayerMovementController : MonoBehaviour
+    {
+
+    }
+
+}
