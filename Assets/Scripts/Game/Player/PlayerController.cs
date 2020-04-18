7e6a1e005b2f80a4dc36bfc2d036ade91f4c18a5
KingVanti <kingvanti650@gmail.com>
2020-04-18 11:16:48 +0200

    Add PlayerController script

---
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.Player
+{
+
+    /// <summary>
+    /// Controls the player
+    /// </summary>
+    public class PlayerController : MonoBehaviour
+    {
+
+    }
+
+}
