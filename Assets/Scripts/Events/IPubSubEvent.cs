0e3f02c64d9cb4ca58bf6d5213ad466bde065177
KingVanti <kingvanti650@gmail.com>
2020-04-18 16:03:58 +0200

    Create IPubSubEvent interface

---
+++ Assets/Scripts/Events/IPubSubEvent.cs
@@ -0,0 +1,15 @@
+﻿namespace Gang1057.Ludiwuri.Events
+{
+
+    /// <summary>
+    /// Interface implemented by all pub/sub events
+    /// </summary>
+    public interface IPubSubEvent { }
+
+    /// <summary>
+    /// Interface implemented by all pub/sub events
+    /// </summary>
+    /// <typeparam name="T">The events payload type</typeparam>
+    public interface IPubSubEvent<T> : IPubSubEvent { }
+
+}
