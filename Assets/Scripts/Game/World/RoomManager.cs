ef281db58e563b7dcf903aa3edab25aeebfe4cf3
KingVanti <kingvanti650@gmail.com>
2020-04-18 16:20:10 +0200

    Add LoadRoom methods

--- Assets/Scripts/Game/World/RoomManager.cs
+++ Assets/Scripts/Game/World/RoomManager.cs
@@ -9,6 +9,35 @@ namespace Gang1057.Ludiwuri.Game.World
     public class RoomManager : MonoBehaviour
     {
 
+        #region Methods
+
+        /// <summary>
+        /// Loads a room with the given name
+        /// </summary>
+        /// <param name="roomName">The rooms name</param>
+        public void LoadRoom(string roomName)
+        {
+            // Get the room asset with requested name
+
+            RoomAsset roomAsset = Resources.Load<RoomAsset>($"Rooms/{roomName}");
+
+            // Load the associated room prefab
+
+            LoadRoom(roomAsset.RoomPrefab);
+        }
+
+
+        /// <summary>
+        /// Loads a room 
+        /// </summary>
+        /// <param name="roomPrefab">The rooms prefab</param>
+        private void LoadRoom(GameObject roomPrefab)
+        {
+            // TODO: Implement room loading
+        }
+
+        #endregion
+
     }
 
 }
