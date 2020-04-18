3d85a1a6aa65bb4f4a3bebe17aae7b54416f02a7
KingVanti <kingvanti650@gmail.com>
2020-04-18 12:33:57 +0200

    Add rb field

--- Assets/Scripts/Game/Player/PlayerMovementController.cs
+++ Assets/Scripts/Game/Player/PlayerMovementController.cs
@@ -9,6 +9,19 @@ namespace Gang1057.Ludiwuri.Game.Player
     public class PlayerMovementController : MonoBehaviour
     {
 
+        #region Fields
+
+#pragma warning disable 649
+
+        /// <summary>
+        /// The rigid-body used for moving the player
+        /// </summary>
+        [SerializeField] private Rigidbody2D rb;
+
+#pragma warning restore 649
+
+        #endregion
+
     }
 
 }
