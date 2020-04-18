adb0c151522b74893c07312cbf48b1efce60fbb6
KingVanti <kingvanti650@gmail.com>
2020-04-18 19:28:39 +0200

    Create ISpawnPointinterface

---
+++ Assets/Scripts/Game/World/ISpawnPoint.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game.World
+{
+
+    /// <summary>
+    /// Implemented by all scripts the player can spawn on
+    /// </summary>
+    public interface ISpawnPoint
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// The position at which to spawn the player
+        /// </summary>
+        Vector2 Position { get; }
+
+        #endregion
+
+    }
+
+}
