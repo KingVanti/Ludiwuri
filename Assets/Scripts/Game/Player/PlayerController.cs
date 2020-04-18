7b6799feb667f18a6bdc9ae0ae640063cb736f3e
KingVanti <kingvanti650@gmail.com>
2020-04-18 12:31:46 +0200

    Add MovementController property

--- Assets/Scripts/Game/Player/PlayerController.cs
+++ Assets/Scripts/Game/Player/PlayerController.cs
@@ -9,6 +9,28 @@ namespace Gang1057.Ludiwuri.Game.Player
     public class PlayerController : MonoBehaviour
     {
 
+        #region Fields
+
+        /// <summary>
+        /// Backing field to <see cref="MovementController"/>
+        /// </summary>
+        [SerializeField] private PlayerMovementController _movementController;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The controller used for player movement
+        /// </summary>
+        public PlayerMovementController MovementController
+        {
+            get { return _movementController; }
+            set { _movementController = value; }
+        }
+
+        #endregion
+
     }
 
 }
