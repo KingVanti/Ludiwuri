c25adf522262bb03ca73eb9219eb36a1f77d4b7e
KingVanti <kingvanti650@gmail.com>
2020-04-18 12:56:17 +0200

    Add speed variables

--- Assets/Scripts/Game/Player/PlayerMovementController.cs
+++ Assets/Scripts/Game/Player/PlayerMovementController.cs
@@ -14,6 +14,14 @@ namespace Gang1057.Ludiwuri.Game.Player
 #pragma warning disable 649
 
         /// <summary>
+        /// The speed with which the player walks
+        /// </summary>
+        [SerializeField] private float walkSpeed;
+        /// <summary>
+        /// The speed with which the player runs
+        /// </summary>
+        [SerializeField] private float runSpeed;
+        /// <summary>
         /// The rigid-body used for moving the player
         /// </summary>
         [SerializeField] private Rigidbody2D rb;
