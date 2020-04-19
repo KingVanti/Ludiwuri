d2212d31f8f2553321b1a24752177367256655b0
KingVanti <kingvanti650@gmail.com>
2020-04-19 11:42:59 +0200

    Cretae MatchMinigameManager class

---
+++ Assets/Scripts/Game/MatchMinigameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gang1057.Ludiwuri.Game
+{
+
+    /// <summary>
+    /// Manages the Match-mini-game
+    /// </summary>
+    public class MatchMinigameManager : MonoBehaviour
+    {
+
+    }
+
+}
