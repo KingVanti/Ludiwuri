f7a7df2744d03185426866089c59681b4850e409
KingVanti <kingvanti650@gmail.com>
2020-04-19 08:19:15 +0200

    Create CandleHealthBar class

---
+++ Assets/Scripts/Game/UI/CandleHealthBar.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Gang1057.Ludiwuri.Game.UI
+{
+
+    /// <summary>
+    /// Displays the lamps health
+    /// </summary>
+    public class CandleHealthBar : MonoBehaviour
+    {
+
+        #region Fields
+
+#pragma warning disable 649
+
+        /// <summary>
+        /// The slider used to display the health
+        /// </summary>
+        [SerializeField] private Slider slider;
+
+#pragma warning restore 649
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Displays the given health value
+        /// </summary>
+        /// <param name="health">The value</param>
+        public void DisplayHealth(int health)
+        {
+            slider.value = health;
+        }
+
+        #endregion
+
+    }
+
+}
