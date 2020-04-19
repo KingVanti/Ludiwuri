a298f79fc95c997589569da6e8efb36eb8701777
KingVanti <kingvanti650@gmail.com>
2020-04-19 09:16:29 +0200

    Create MasterPlayerController class

---
+++ Assets/Scripts/Game/Player/MasterPlayerController.cs
@@ -0,0 +1,33 @@
+﻿namespace Gang1057.Ludiwuri.Game.Player
+{
+
+    /// <summary>
+    /// Master controller for the player
+    /// </summary>
+    public class MasterPlayerController
+    {
+
+        #region Fields
+
+        /// <summary>
+        /// Backing field to <see cref="CurrentPlayerController"/>
+        /// </summary>
+        private PlayerController _curentPlayerController;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// The currently active player controller
+        /// </summary>
+        public PlayerController CurrentPlayerController
+        {
+            get { return _curentPlayerController; }
+            set { _curentPlayerController = value; }
+        }
+
+        #endregion
+
+    }
+}
