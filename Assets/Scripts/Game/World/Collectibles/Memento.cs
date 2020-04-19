2f6ad58ba2b7194573d2087b0b50f4cb5a6fb74f
KingVanti <kingvanti650@gmail.com>
2020-04-19 08:24:43 +0200

    Create Memento class

---
+++ Assets/Scripts/Game/World/Collectibles/Memento.cs
@@ -0,0 +1,22 @@
+﻿namespace Gang1057.Ludiwuri.Game.World.Collectibles
+{
+
+    /// <summary>
+    /// A memento the player must collect
+    /// </summary>
+    public class Memento : Collectible
+    {
+
+        #region Methods
+
+        /// <inheritdoc/>
+        protected override void OnCollected()
+        {
+            // TODO: Add memento to player inventory
+        }
+
+        #endregion
+
+    }
+
+}
