ab278881ed3f10d94bd72b4cb5965b3b65e79eb3
KingVanti <kingvanti650@gmail.com>
2020-04-19 09:42:15 +0200

    Implement candle lighting

--- Assets/Scripts/Game/World/Collectibles/Candle.cs
+++ Assets/Scripts/Game/World/Collectibles/Candle.cs
@@ -1,4 +1,7 @@
-﻿namespace Gang1057.Ludiwuri.Game.World.Collectibles
+﻿using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Gang1057.Ludiwuri.Game.World.Collectibles
 {
 
     /// <summary>
@@ -6,6 +9,36 @@
     /// </summary>
     public class Candle : Collectible
     {
+
+        #region Fields
+
+#pragma warning disable 649
+
+        /// <summary>
+        /// The candles light source
+        /// </summary>
+        [SerializeField] private Light2D source;
+
+#pragma warning restore 649
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Sets whether the candle is lit
+        /// </summary>
+        public bool Lit
+        {
+            set
+            {
+                source.enabled = value;
+
+                // TODO: Update animator
+            }
+        }
+
+        #endregion
 
         #region Methods
 
