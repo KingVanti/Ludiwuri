4a6b3e3412fc8fd8a2b2d4101a94501075a597f4
KingVanti <kingvanti650@gmail.com>
2020-04-19 17:03:15 +0200

    Create WallCandle class

---
+++ Assets/Scripts/Game/World/WallCandle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
+
+namespace Gang1057.Ludiwuri.Game.World
+{
+
+    /// <summary>
+    /// A candle that is mounted to the wall
+    /// </summary>
+    public class WallCandle : MonoBehaviour, IInteractable
+    {
+
+        #region Fields
+
+        /// <summary>
+        /// The candles light source
+        /// </summary>
+        [SerializeField] private Light2D lightSource;
+
+        #endregion
+
+        #region Methods
+
+        /// <inheritdoc/>
+        public void Interact()
+        {
+            // TODO: Light candle
+        }
+
+        #endregion
+
+    }
+
+}
