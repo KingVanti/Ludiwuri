d48fc48e0d7ee8c7230be15c0e581e6a2083a98d
KingVanti <kingvanti650@gmail.com>
2020-04-19 17:35:43 +0200

    Update IInteractable.cs

--- Assets/Scripts/Game/World/IInteractable.cs
+++ Assets/Scripts/Game/World/IInteractable.cs
@@ -7,8 +7,20 @@
     public interface IInteractable
     {
 
+        #region Properties
+
+        /// <summary>
+        /// Indicates whether this intractable is currently interactable
+        /// </summary>
+        public bool Interactable { get; }
+
+        #endregion
+
         #region Methods
 
+        /// <summary>
+        /// Interacts with the object
+        /// </summary>
         void Interact();
 
         #endregion
