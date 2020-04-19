b95f5be933ceb6751a5d69aac35d84eafe055ec6
KingVanti <kingvanti650@gmail.com>
2020-04-19 07:47:24 +0200

    Implement player guarding

--- Assets/Scripts/Game/Player/WithLampPlayerController.cs
+++ Assets/Scripts/Game/Player/WithLampPlayerController.cs
@@ -6,8 +6,53 @@ namespace Gang1057.Ludiwuri.Game.Player
     /// <summary>
     /// Controls the player while he has a lamp
     /// </summary>
-    public class WithLampPlayerController : MonoBehaviour
+    public class WithLampPlayerController : PlayerController
     {
+
+        #region Fields
+
+        /// <summary>
+        /// Backing field to <see cref="Guarding"/>
+        /// </summary>
+        private bool _guarding;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Whether the player is currently guarding the candle
+        /// </summary>
+        public bool Guarding
+        {
+            get { return _guarding; }
+            set
+            {
+                _guarding = value;
+
+                // TODO: Update animator
+                // TODO: Set candle invincible
+
+                MovementController.Locked = value;
+            }
+        }
+
+        #endregion
+
+
+        #region Methods
+
+        private void Update()
+        {
+            // Update "Guarding" property
+
+            bool guarding = Input.GetButton("Guard");
+
+            if (guarding != Guarding)
+                Guarding = guarding;
+        }
+
+        #endregion
 
     }
 
