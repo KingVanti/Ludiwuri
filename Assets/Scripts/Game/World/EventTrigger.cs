0d64b6fdff32a8459525fe6919b2bf2650a0d961
KingVanti <kingvanti650@gmail.com>
2020-04-19 09:46:04 +0200

    Create EventTrigger class

---
+++ Assets/Scripts/Game/World/EventTrigger.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// Used to detect the player
+/// </summary>
+public class EventTrigger : MonoBehaviour
+{
+
+    #region Fields
+
+    /// <summary>
+    /// Called when the trigger is entered
+    /// </summary>
+    public UnityEvent onEnter;
+    /// <summary>
+    /// Called when the trigger is exited
+    /// </summary>
+    public UnityEvent onExit;
+
+    #endregion
+
+    #region Methods
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        onEnter.Invoke();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        onExit.Invoke();
+    }
+
+    #endregion
+
+}
