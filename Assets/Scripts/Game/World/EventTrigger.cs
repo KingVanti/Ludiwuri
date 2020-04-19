6ca48bcc0688baaba0347cbe274abefa35e2a93b
KingVanti <kingvanti650@gmail.com>
2020-04-19 09:50:49 +0200

    Update EventTrigger class

--- Assets/Scripts/Game/World/EventTrigger.cs
+++ Assets/Scripts/Game/World/EventTrigger.cs
@@ -13,10 +13,20 @@ public class EventTrigger : MonoBehaviour
     /// Called when the trigger is entered
     /// </summary>
     public UnityEvent onEnter;
+
+#pragma warning disable 649
+
     /// <summary>
-    /// Called when the trigger is exited
+    /// Whether the trigger should only fire once
     /// </summary>
-    public UnityEvent onExit;
+    [SerializeField] private bool once = true;
+
+#pragma warning restore 649
+
+    /// <summary>
+    /// Whether the trigger was already triggered
+    /// </summary>
+    private bool triggered;
 
     #endregion
 
@@ -24,12 +34,11 @@ public class EventTrigger : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        onEnter.Invoke();
-    }
-
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        onExit.Invoke();
+        if (!once || !triggered)
+        {
+            onEnter.Invoke();
+            triggered = true;
+        }
     }
 
     #endregion
