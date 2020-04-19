bd2f7fd0df3a45206910f3f7de26bd27c33a90d4
KingVanti <kingvanti650@gmail.com>
2020-04-19 08:26:39 +0200

    Create Candle class

---
+++ Assets/Scripts/Game/World/Collectibles/Candle.cs
@@ -0,0 +1,22 @@
+﻿namespace Gang1057.Ludiwuri.Game.World.Collectibles
+{
+
+    /// <summary>
+    /// Gives the player a candle when collected
+    /// </summary>
+    public class Candle : Collectible
+    {
+
+        #region Methods
+
+        /// <inheritdoc/>
+        protected override void OnCollected()
+        {
+            // TODO: Give player candle
+        }
+
+        #endregion
+
+    }
+
+}
