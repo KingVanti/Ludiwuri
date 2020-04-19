00f0f2f094855071e70f6de22b92274d184f4dc5
KingVanti <kingvanti650@gmail.com>
2020-04-19 18:31:18 +0200

    Create DeathCountdownText

---
+++ Assets/Scripts/Game/UI/DeathCountdownText.cs
@@ -0,0 +1,29 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class DeathCountdownText : MonoBehaviour
+{
+
+    #region Fields
+
+#pragma warning disable 649
+
+    [SerializeField] private TextMeshProUGUI text;
+
+#pragma warning restore 649
+
+    #endregion
+
+    #region Methods
+
+    public void SetCountdownTime(int? time)
+    {
+        if (time.HasValue)
+            text.text = $"{time.Value} second{(time.Value != 1 ? "s" : "")}";
+        else
+            text.text = "";
+    }
+
+    #endregion
+
+}
