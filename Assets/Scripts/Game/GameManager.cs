936e56054cfe844a618cb6d7540cf57fe05e80a5
KingVanti <kingvanti650@gmail.com>
2020-04-19 18:37:24 +0200

    Implement wintime

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
 
 namespace Gang1057.Ludiwuri.Game
 {
@@ -9,6 +11,55 @@ namespace Gang1057.Ludiwuri.Game
     public class GameManager : MonoBehaviour, ISceneManager
     {
 
+        #region Fields
+
+#pragma warning disable 649
+
+        [SerializeField] private int gameSeconds;
+        [SerializeField] private TextMeshProUGUI winTimeText;
+
+#pragma warning restore 649
+
+        private int _secondsToWin;
+
+        #endregion
+
+        #region Properties
+
+        public int SecondsToWin
+        {
+            get { return _secondsToWin; }
+            set
+            {
+                _secondsToWin = value;
+
+                winTimeText.text = $"{(value / 60).ToString("00")}:{(value % 60).ToString("00")}";
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void Awake()
+        {
+            SecondsToWin = gameSeconds;
+
+            StartCoroutine(TickTime());
+        }
+
+        private IEnumerator TickTime()
+        {
+            while (SecondsToWin > 0)
+            {
+                yield return new WaitForSeconds(1);
+                SecondsToWin--;
+            }
+
+            // TODO: Trigger Win
+        }
+
+        #endregion
 
     }
 
