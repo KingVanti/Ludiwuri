8c944fe0fd14b900fa4f0c4bd633586f4a85a8d4
KingVanti <kingvanti650@gmail.com>
2020-04-20 07:51:25 +0200

    Update Candle.cs

--- Assets/Scripts/Game/Player/Candle.cs
+++ Assets/Scripts/Game/Player/Candle.cs
@@ -43,6 +43,7 @@ namespace Gang1057.Ludiwuri.Game.Player
                 lightSource.enabled = value;
 
                 controller.RefreshDeathCountdownCondition();
+                controller.RefreshInteractionPrompt();
             }
         }
 
