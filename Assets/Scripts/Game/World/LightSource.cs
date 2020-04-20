1c981f7cb47eda5d7e5d949b0bda697fa317f032
KingVanti <kingvanti650@gmail.com>
2020-04-20 15:10:04 +0200

    Implement lit candle animation

--- Assets/Scripts/Game/World/LightSource.cs
+++ Assets/Scripts/Game/World/LightSource.cs
@@ -9,11 +9,17 @@ namespace Gang1057.Ludiwuri.Game.World
 
         #region Fields
 
+#pragma warning disable 649
+
         /// <summary>
         /// The source
         /// </summary>
-        [SerializeField] protected Light2D lightSource;
-        private bool _lit = true;
+        [SerializeField] private Light2D lightSource;
+        [SerializeField] private Animator anim;
+
+#pragma warning restore 649
+
+        private bool _lit;
 
         #endregion
 
@@ -29,10 +35,20 @@ namespace Gang1057.Ludiwuri.Game.World
             {
                 _lit = value;
                 lightSource.enabled = value;
+                anim.SetBool("Lit", value);
             }
         }
 
         public float Radius { get { return lightSource.pointLightOuterRadius * 0.5f; } }
+
+        #endregion
+
+        #region Methods
+
+        private void Awake()
+        {
+            Lit = true;
+        }
 
         #endregion
 
